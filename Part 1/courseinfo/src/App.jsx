cd5504d62812c0bc1276fbd601ff137f09ec5069
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-30 12:12:47 +0545

    feat: Added courseinfo component

---
+++ Part 1/courseinfo/src/App.jsx
@@ -0,0 +1,56 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header=(props) =>{
+    return(
+      <>
+      <h1>{props.course}</h1>
+      </>
+    )
+  }
+
+  const Content = (props) =>{
+    return(
+      <>
+      <p>{props.pt} {props.ex}</p>
+      </>
+    )
+  } 
+
+  const Total =(props)=>{
+    return(
+      <>
+    <p>{props.ttl} {props.totl}</p>
+    </>
+    )
+  }
+
+  return (
+    <>
+      {/* <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
+      <Header course= {course}/>
+      <Content pt ={part1} ex={exercises1} />
+      <Content pt ={part2} ex={exercises2} />
+      <Content pt ={part3} ex={exercises3} />
+      <Total ttl='Number of exercise' totl={exercises1+exercises3+exercises3} />
+    </>
+  )
+}
+
+export default App
