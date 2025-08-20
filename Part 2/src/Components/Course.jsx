2f98748edaa4b1e7c3163a3e173948749c684e78
Lk-Chy <deathreaperzeref@gmail.com>
2025-08-20 19:51:35 +0545

    break down the codes into components structures

---
+++ Part 2/src/Components/Course.jsx
@@ -0,0 +1,15 @@
+import Content from "./Content"
+import Header from "./Header"
+import Sum from "./Sum"
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Sum parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
